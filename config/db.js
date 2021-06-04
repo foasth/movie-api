299fc7fde970af8b07f002d626be0bf7ececafe1
foasth <ankhoud@yahoo.fr>
2021-06-04 10:12:08 +0100

    update

---
+++ config/db.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb+srv://admin:admin@cluster0.b55wd.mongodb.net/moviedb?retryWrites=true&w=majority', {useNewUrlParser: true},
+    (err) => {
+        if (!err) {
+            console.log('Successfully Established Connection with MongoDB')
+        } else {
+            console.log('Failed to Establish Connection with MongoDB with Error: ' + err)
+        }
+    });
+module.exports = mongoose
+
---
+++ schemas/movie.js
@@ -0,0 +1,15 @@
+const mongoose =require ('mongoose')
+
+const movieSchema = mongoose.Schema({
+
+    title:{
+        type: String
+    },
+    description:{
+        type: String
+    },
+    rating:{
+        type: Number
+    }
+})
+module.exports = mongoose.model('Movie',movieSchema)
